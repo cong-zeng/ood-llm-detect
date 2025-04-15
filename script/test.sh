4e0d6774bab852a3b856ce55b9bf5c635b9f4f12
cong-zeng <cong-zeng66@gmail.com>
2025-04-15 20:28:33 +0400

    update test knn script for m4

--- script/test.sh
+++ script/test.sh
@@ -1,8 +1,10 @@
 DATA_PATH="data" #PATH for the data
-Model_PATH="Deepfake_best.pth" #PATH for the model ckpt
-export CUDA_VISIBLE_DEVICES=1,2,3,4,5
+# Model_PATH="ckpt/M4_multilingual_best.pth" #PATH for the model ckpt
+Model_PATH="ckpt/Deepfake_best.pth" #PATH for the model ckpt
+
+export CUDA_VISIBLE_DEVICES=0,1
 # deepfake
-python test_knn.py --device_num 5 --batch_size 128 --max_K 5 --model_name princeton-nlp/unsup-simcse-roberta-base \
+python test_knn.py --device_num 2 --batch_size 128 --max_K 5 --model_name princeton-nlp/unsup-simcse-roberta-base \
                    --mode deepfake --database_path ${DATA_PATH}/Deepfake/cross_domains_cross_models --database_name 'train' \
                    --test_dataset_path ${DATA_PATH}/Deepfake/cross_domains_cross_models --test_dataset_name 'test'\
                    --model_path ${Model_PATH} --save_database --save_path database/deepfake
@@ -20,7 +22,7 @@ python test_knn.py --device_num 5 --batch_size 128 --max_K 5 --model_name prince
 #                    --model_path ${Model_PATH} --save_database --save_path database/M4-monolingual
 
 # # M4-multilingual
-# python test_knn.py --device_num 8 --batch_size 128 --max_K 5 --model_name princeton-nlp/unsup-simcse-roberta-base \
+# python test_knn.py --device_num 2 --batch_size 128 --max_K 5 --model_name princeton-nlp/unsup-simcse-roberta-base \
 #                    --mode M4 --database_path ${DATA_PATH}/SemEval2024-M4/SubtaskA --database_name 'multilingual_train' \
 #                    --test_dataset_path ${DATA_PATH}/SemEval2024-M4/SubtaskA --test_dataset_name 'multilingual_test'\
 #                    --model_path ${Model_PATH} --save_database --save_path database/M4-multilingual
