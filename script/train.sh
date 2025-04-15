54b895592551543446528660b3969e512dbcf679
cong-zeng <cong-zeng66@gmail.com>
2025-04-15 20:28:56 +0400

    train energy on M4

--- script/train.sh
+++ script/train.sh
@@ -77,16 +77,16 @@ export CUDA_VISIBLE_DEVICES=1
 #     --savedir /home/zc/DeTeCtive/runs/deepfake-roberta-base_v1
 
 # M4-multilingual
-python train_classifier_hrn.py --device_num 1 --per_gpu_batch_size 32 --total_epoch 10 --lr 2e-5 --warmup_steps 2000\
-    --method hrn\
-    --classifier_dim 1\
-    --only_classifier\
-    --resum True\
-    --pth_path /home/zc/DeTeCtive/ckpt/M4_multilingual_best.pth\
-    --model_name princeton-nlp/unsup-simcse-roberta-base --dataset M4 --path ${DATA_PATH}/SemEval2024-M4/SubtaskA \
-    --name M4-multilingual-roberta-base --freeze_embedding_layer --database_name multilingual_train --test_dataset_name multilingual_test\
-    --skip_train\
-    --savedir /home/zc/DeTeCtive/runs/M4-multilingual-roberta-base_v5
+# python train_classifier_hrn.py --device_num 1 --per_gpu_batch_size 32 --total_epoch 10 --lr 2e-5 --warmup_steps 2000\
+#     --method hrn\
+#     --classifier_dim 1\
+#     --only_classifier\
+#     --resum True\
+#     --pth_path /home/zc/DeTeCtive/ckpt/M4_multilingual_best.pth\
+#     --model_name princeton-nlp/unsup-simcse-roberta-base --dataset M4 --path ${DATA_PATH}/SemEval2024-M4/SubtaskA \
+#     --name M4-multilingual-roberta-base --freeze_embedding_layer --database_name multilingual_train --test_dataset_name multilingual_test\
+#     --skip_train\
+#     --savedir /home/zc/DeTeCtive/runs/M4-multilingual-roberta-base_v5
 
 #################################################################################
 #                                   Energy                                      #
@@ -102,11 +102,11 @@ python train_classifier_hrn.py --device_num 1 --per_gpu_batch_size 32 --total_ep
 #     --name deepfake-roberta-base --freeze_embedding_layer --database_name train --test_dataset_name test\
 
 # M4-multilingual
-# python train_classifier_energy.py --device_num 1 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
-#     --method energy\
-#     --classifier_dim 7\
-#     --only_classifier\
-#     --resum True\
-#     --pth_path /home/zc/DeTeCtive/ckpt/M4_multilingual_best.pth\
-#     --model_name princeton-nlp/unsup-simcse-roberta-base --dataset M4 --path ${DATA_PATH}/SemEval2024-M4/SubtaskA \
-#     --name M4-multilingual-roberta-base --freeze_embedding_layer --database_name multilingual_train --test_dataset_name multilingual_test
+python train_classifier_energy.py --device_num 1 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
+    --method energy\
+    --classifier_dim 5\
+    --only_classifier\
+    --resum True\
+    --pth_path /home/zc/DeTeCtive/ckpt/M4_multilingual_best.pth\
+    --model_name princeton-nlp/unsup-simcse-roberta-base --dataset M4 --path ${DATA_PATH}/SemEval2024-M4/SubtaskA \
+    --name M4-multilingual-roberta-base --freeze_embedding_layer --database_name multilingual_train --test_dataset_name multilingual_test
