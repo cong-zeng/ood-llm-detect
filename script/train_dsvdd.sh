c5424ba09da767455fa4974d715cf53c0742c932
cong-zeng <cong-zeng66@gmail.com>
2025-10-08 06:32:35 +0400

    add local infer demo;add readme

--- script/train_dsvdd.sh
+++ script/train_dsvdd.sh
@@ -14,19 +14,19 @@ python train_classifier_dsvdd.py --device_num 2 --per_gpu_batch_size 32 --total_
     --name deepfake-roberta-base --freeze_embedding_layer --database_name train --test_dataset_name val
 
 # M4-multilingual
-# python train_classifier_dsvdd.py --device_num 2 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
-#     --method dsvdd\
-#     --out_dim 768\
-#     --one_loss\
-#     --objective one-class\
-#     --model_name princeton-nlp/unsup-simcse-roberta-base --dataset M4 --path ${DATA_PATH}/SemEval2024-M4/SubtaskA \
-#     --name M4-multilingual-roberta-base --freeze_embedding_layer --database_name multilingual_train --test_dataset_name multilingual_test
+python train_classifier_dsvdd.py --device_num 2 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
+    --method dsvdd\
+    --out_dim 768\
+    --one_loss\
+    --objective one-class\
+    --model_name princeton-nlp/unsup-simcse-roberta-base --dataset M4 --path ${DATA_PATH}/SemEval2024-M4/SubtaskA \
+    --name M4-multilingual-roberta-base --freeze_embedding_layer --database_name multilingual_train --test_dataset_name multilingual_test
 
 # raid
-# python train_classifier_dsvdd.py --device_num 2 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 1000\
-#     --method dsvdd\
-#     --out_dim 768\
-#     --one_loss\
-#     --objective one-class\
-#     --model_name princeton-nlp/unsup-simcse-roberta-base --dataset raid \
-#     --name raid-roberta-base --freeze_embedding_layer --database_name train --test_dataset_name val
+python train_classifier_dsvdd.py --device_num 2 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 1000\
+    --method dsvdd\
+    --out_dim 768\
+    --one_loss\
+    --objective one-class\
+    --model_name princeton-nlp/unsup-simcse-roberta-base --dataset raid \
+    --name raid-roberta-base --freeze_embedding_layer --database_name train --test_dataset_name val
--- script/train_energy.sh
+++ script/train_energy.sh
@@ -4,6 +4,11 @@ export CUDA_VISIBLE_DEVICES=0,1
 #################################################################################
 #                                   Energy                                      #
 #################################################################################
+
+# Since Energy is based on weight pretrained by DeTecCtive, We need to first download the weights
+# For Deepfake dataset
+wget -c https://huggingface.co/heyongxin233/DeTeCtive/resolve/main/Deepfake_best.pth ./ckpt/Deepfake_best.pth
+
 # deepfake
 Model_pth=""
 python train_classifier_energy.py --device_num 2 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 1000\
@@ -14,6 +19,10 @@ python train_classifier_energy.py --device_num 2 --per_gpu_batch_size 32 --total
     --resum True\
     --pth_path ${Model_pth}\
 
+# For M4 dataset
+wget -c https://huggingface.co/heyongxin233/DeTeCtive/resolve/main/M4_multilingual_best.pth -c ./ckpt/M4_multilingual_best.pth 
+# # M4-multilingual
+pretrained_pth="./ckpt/M4_multilingual_best.pth"
 # M4-multilingual
 Model_pth=""
 python train_classifier_energy.py --device_num 2 --per_gpu_batch_size 32 --total_epoch 50 --lr 2e-5 --warmup_steps 2000\
@@ -26,7 +35,8 @@ python train_classifier_energy.py --device_num 2 --per_gpu_batch_size 32 --total
     --name M4-multilingual-roberta-base --freeze_embedding_layer --database_name multilingual_train --test_dataset_name multilingual_test
 
 # raid
-Model_pth=""
+wget -c https://huggingface.co/Shengkun/ood-detection/resolve/main/model_raid.pth -c ./ckpt/model_raid.pth
+Model_pth="./ckpt/model_raid.pth"
 python train_classifier_energy.py --device_num 2 --per_gpu_batch_size 64 --total_epoch 50 --lr 2e-5 --warmup_steps 1000\
     --method energy\
     --classifier_dim 6\
